585194a991b40dae5a5a474d05d85de36bef274b
oreolion <remyoreo11@gmail.com>
2024-05-28 04:58:04 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import Header from "./components/Header";
+
+const Home = async () => {
+  return (
+    <>
+      <Header />
+      <div className="bg-slate-300 bg-gradient-conic-full max-w-[85rem] h-full px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <Link
+            key={""}
+            className="group flex flex-col h-full bg-white border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 "
+            href={""}>
+            <div className="aspect-w-16 aspect-h-11">
+              <Image
+                className="object-cover h-48 w-96 rounded-xl"
+                src={`https://source.unsplash.com/featured/?${encodeURIComponent(
+                  "hello world"
+                )}`}
+                width={500}
+                height={500}
+                alt="Image Description"
+              />
+            </div>
+            <div className="my-6">
+              <h3 className="text-xl font-semibold text-gray-800 ">
+                Hello World
+              </h3>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/app/writearticle/page.tsx
@@ -0,0 +1,11 @@
+import { Article } from "../components/Article";
+import Header from "../components/Header";
+
+export default function WriteArticle() {
+  return (
+    <>
+      <Header />
+      <Article />
+    </>
+  );
+}
